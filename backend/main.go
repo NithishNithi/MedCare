14b089fda61264a0c84e7ec4a6a0bcba64eaf3b6
NithishNithi <nithishthangarasu02@gmail.com>
2024-02-26 23:37:33 +0530

    Updated

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"backend/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+
+func InitDatabase(){
+	
+}
+
 func main() {
 	r := gin.Default()
 	routes.SetupRoutes(r)
+	log.Fatal(r.Run(":8080"))
 }
