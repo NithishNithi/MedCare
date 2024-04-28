f4834b051e25ec8fbb62e81c2403c8cbc2a21c93
NithishNithi <nithishthangarasu02@gmail.com>
2024-04-28 13:40:41 +0530

    Routes Updated

--- constants/constant.go
+++ constants/constant.go
@@ -1,6 +1,6 @@
 package constants
 
 const SecretKey = "&&/)hNbhIe0r;{<P}D,}{KRwWi06I,CP0TXDKW&?i5idnJ$HkI4U_Eacxvc_,;c"
-const MongoURI = "mongodb+srv://mrnithisht:mrnithisht@nithi1.ufjvb6g.mongodb.net/admin?retryWrites=true&loadBalanced=false&replicaSet=atlas-yxjy4i-shard-0&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"
+const MongoURI = "mongodb://localhost:27017/"
 const Mailgun_apikey = "aa2c1100464e22845aebd3b68920dad6-2c441066-1cf5bad6"
 const Gemini_apiKey = "AIzaSyB2DZif4zaTmF8q7VuHl5p4N_7lN7r0e-s"
